c34a79c44fc0efe126bbb4b57d35b722fc50a32a
a23jonam <a23jonam@student.his.se>
2025-02-17 15:43:33 +0100

    New layout manager
    Added a boxlayout in main and panels to allow that both home tasks and study tasks are displayed in the gui

--- src/assignment2/ToDo.java
+++ src/assignment2/ToDo.java
@@ -1,21 +1,28 @@
 package assignment2;
 
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import se.his.it401g.todo.HomeTask;
-//import se.his.it401g.todo.StudyTask;
+import se.his.it401g.todo.StudyTask;
 import se.his.it401g.todo.Task;
 
 public class ToDo {
-
 	public static void main(String[] args) {
 		/*ToDo application = new ToDo();
 		application.execute();*/	
-		JFrame frame = new JFrame();
+		JFrame frame = new JFrame("ToDo");
 		
-		Task task = new HomeTask();
+		JPanel panel = new JPanel();
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		
+		Task homeTask = new HomeTask();
+		Task studyTask = new StudyTask();
 		//Task task1 = new StudyTask();
-		frame.add(task.getGuiComponent());
+		panel.add(homeTask.getGuiComponent());
+		panel.add(studyTask.getGuiComponent());
+		frame.add(panel);
 		frame.setBounds(100,100,400,100);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
