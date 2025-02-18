8b33e3b274281853f5269fc1773aca96a8a2f387
Smorsas51 <a23folan@student.his.se>
2025-02-18 15:33:07 +0100

    progressbar display done

---
+++ src/assignment2/PBar.java
@@ -0,0 +1,73 @@
+package assignment2;
+
+
+import se.his.it401g.todo.Task;
+import se.his.it401g.todo.TaskListener;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+
+public class PBar extends JPanel implements TaskListener {
+    private JProgressBar progressBar;
+    private JLabel completedLabel;
+    private List<Task> taskList;
+    private int completedTasks = 0;
+
+
+    public PBar(List<Task> tasks) {
+    	this.taskList = tasks;
+        setLayout(new BorderLayout());        
+        
+        completedLabel = new JLabel("0 / " + taskList.size() + " tasks completed", SwingConstants.CENTER);        add(completedLabel, BorderLayout.CENTER);
+        
+        add(completedLabel, BorderLayout.CENTER);
+        updateProgress();
+    }
+
+    private void updateProgress() {
+        if (taskList.isEmpty()) {
+            completedLabel.setText("0 / 0 tasks completed");
+            return;
+        }
+
+        completedLabel.setText(completedTasks + " / " + taskList.size() + " tasks completed");
+    }
+
+    @Override
+    public void taskCompleted(Task t) {
+        if (!t.isComplete()) {
+            completedTasks++;
+        }
+        updateProgress();
+    }
+
+    @Override
+    public void taskUncompleted(Task t) {
+        if (t.isComplete()) {
+            completedTasks--;
+        }
+        updateProgress();
+    }
+
+    @Override
+    public void taskChanged(Task t) {
+        updateProgress();
+    }
+
+    @Override
+    public void taskRemoved(Task t) {
+        if (taskList.remove(t) && t.isComplete()) {
+            completedTasks--;
+        }
+        updateProgress();
+    }
+
+	@Override
+	public void taskCreated(Task t) {
+        taskList.add(t);
+        updateProgress();
+		
+	}
+
+}
--- src/assignment2/ToDo.java
+++ src/assignment2/ToDo.java
@@ -1,12 +1,18 @@
 package assignment2;
+import assignment2.PBar;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import se.his.it401g.todo.HomeTask;
 import se.his.it401g.todo.StudyTask;
 import se.his.it401g.todo.Task;
+import se.his.it401g.todo.TaskListener;
 
 public class ToDo {
 	public static void main(String[] args) {
@@ -15,16 +21,22 @@ public class ToDo {
 		JFrame frame = new JFrame("ToDo");
 		
 		JPanel panel = new JPanel();
+		
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 		
 		Task homeTask = new HomeTask();
 		Task studyTask = new StudyTask();
 		Task customTask = new CustomTask();
-		//Task task1 = new StudyTask();
+		
+		List<Task> tasks = new ArrayList<>();
 		panel.add(homeTask.getGuiComponent());
 		panel.add(studyTask.getGuiComponent());
 		panel.add(customTask.getGuiComponent());
-		frame.add(panel);
+		
+        PBar progressPanel = new PBar(tasks);
+        frame.add(panel, BorderLayout.CENTER);  
+        frame.add(progressPanel, BorderLayout.SOUTH);
+		
 		frame.setBounds(100,100,400,100);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
