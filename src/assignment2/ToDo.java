7a3583f327c33fe30671568172ad7204b9563d9f
a23jonam <a23jonam@student.his.se>
2025-02-18 15:01:35 +0100

    Added new panel
    The gui now displays buttons on top of the tasks with options to add new tasks. The functionality for the buttons have not yet been implemented

--- src/assignment2/ToDo.java
+++ src/assignment2/ToDo.java
@@ -1,8 +1,15 @@
 package assignment2;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 import se.his.it401g.todo.HomeTask;
 import se.his.it401g.todo.StudyTask;
@@ -10,24 +17,41 @@ import se.his.it401g.todo.Task;
 
 public class ToDo {
 	public static void main(String[] args) {
-		/*ToDo application = new ToDo();
-		application.execute();*/	
-		JFrame frame = new JFrame("ToDo");
+		JFrame frame = new JFrame("To-do List");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400,300);
+		frame.setLayout(new BorderLayout());
 		
-		JPanel panel = new JPanel();
-		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
+		buttonPanel.setPreferredSize(new Dimension(400, 50));
+		//JLabel buttonLabel = new JLabel("BUTTONS");
+		//buttonPanel.add(buttonLabel);
+		buttonPanel.add(new JButton("+ Home Task"));
+		buttonPanel.add(new JButton("+ Study Task"));
+		buttonPanel.add(new JButton("+ Counter Task"));
+		
+		JPanel taskPanel = new JPanel();
+        taskPanel.setLayout(new BoxLayout(taskPanel, BoxLayout.Y_AXIS));
+        taskPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		
+		taskPanel.setLayout(new BoxLayout(taskPanel, BoxLayout.Y_AXIS));
+		//JLabel taskLabel = new JLabel("TASKS");
+		//taskPanel.add(taskLabel);
+		taskPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 		
 		Task homeTask = new HomeTask();
 		Task studyTask = new StudyTask();
 		Task customTask = new CustomTask();
-		//Task task1 = new StudyTask();
-		panel.add(homeTask.getGuiComponent());
-		panel.add(studyTask.getGuiComponent());
-		panel.add(customTask.getGuiComponent());
-		frame.add(panel);
-		frame.setBounds(100,100,400,100);
-		frame.setVisible(true);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+
+		taskPanel.add(homeTask.getGuiComponent());
+		taskPanel.add(studyTask.getGuiComponent());
+		taskPanel.add(customTask.getGuiComponent());
+		
+        frame.add(buttonPanel, BorderLayout.NORTH); // Buttons at top
+        frame.add(new JScrollPane(taskPanel), BorderLayout.CENTER); // Scrollable task list
+
+        frame.setVisible(true);
+    }
 
 }
