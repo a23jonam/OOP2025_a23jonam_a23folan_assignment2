6ddac79cb1b983a7ef35eef33caabd94c805bf4c
Smorsas51 <a23folan@student.his.se>
2025-02-21 13:50:40 +0100

    Push again
    push failed

--- src/assignment2/PBar.java
+++ src/assignment2/PBar.java
@@ -19,7 +19,7 @@ public class PBar extends JPanel implements TaskListener {
     	this.taskList = tasks;
         setLayout(new BorderLayout());        
 
-        completedLabel = new JLabel("0 / " + taskList.size() + " tasks completed", SwingConstants.CENTER);
+        completedLabel = new JLabel(completedTasks + "0 / " + taskList.size() + " tasks completed", SwingConstants.CENTER);
         add(completedLabel, BorderLayout.CENTER);
         updateProgress();
     }
